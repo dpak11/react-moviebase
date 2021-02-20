cdd6c2146a5f2800cdbef581479cd9844a12f74a
deepak <deepakb@protonmail.com>
2021-02-20 17:46:18 +0530

    movie detail - rating color dynamic

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -11,21 +11,27 @@ const MovieDetail = ({ match }) => {
     alignItems: "center",
     justifyContent: "center",
   };
+  const ratingText = {
+    fontSize:"2.4em",
+    fontWeight:"bold"
+  };
   const genres = movie.genre.split("|");
+  let colorRate = Number(movie.rating) > 50 ? "rate-grey" : "rate-pale";
+  colorRate = Number(movie.rating) >= 85 ? "rate-red" : colorRate;
   return (
     <div style={myStyle}>
       <div>
         <h4 style={{color:"grey"}}>MOVIE DETAILS:</h4>
-        <h1>{movie.name}</h1>        
+        <h1 style={{borderBottom: "dotted 2px gray"}}>{movie.name}</h1>        
         <p>
           {genres.map((genre,i) => (
             <span key={i} className="tags" style={{marginRight:"10px"}}>Genre: {genre}</span>
           ))}
         </p>
-        <p>Run Time: {movie.runtime}</p>
-        <p>Release Date: {movie.release}</p>
-        <p>Country: {movie.country}</p>
-        <p>Rating: {movie.rating}%</p>
+        <p>Run Time: <b>{movie.runtime}</b></p>
+        <p>Release Date: <b>{movie.release}</b></p>
+        <p>Country: <b>{movie.country}</b></p>
+        <p><span className={colorRate} style={ratingText}>{movie.rating}%</span></p>
         <Link to="/gallery">&lt; Go Back</Link>
       </div>
     </div>
