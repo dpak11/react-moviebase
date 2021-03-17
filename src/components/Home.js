d1bed1adf2870ecaca597abbf7791aa23b26b142
deepak <deepakb@protonmail.com>
2021-03-17 21:34:31 +0530

    home component changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,30 @@
-//import { useRef } from "react";
+import { Redirect } from "react-router";
+
 const Home = () => {
-  //const jsonRef = useRef();
-  const style = {
+  const homestyle = {
     height: "80vh",
     backgroundColor: "#2D7064",
     fontSize: ".9em",
     padding: "20px 40px",
     color: "#d8cfcf",
   };
-  console.log("home rendererd");
-  /* const fetchData = () => {
-    //fetch("https://idhunammakadai.in/ionic-test/test-ionic-movie.php")
-    fetch("test-ionic-movie.php")
-      .then((data) => data.json())
-      .then((json) => {
-        console.log(json);
-        jsonRef.current.innerHTML = JSON.stringify(json, null, 4);
-      })
-      .catch((err) => {
-        console.log("unable to load");
-        jsonRef.current.innerHTML = "Sorry, unable to fetch";
-      });
-  }; */
 
   return (
-    <div style={style}>
+    <div style={homestyle}>
       <h1>Welcome to React Movie Base (Homepage)</h1>
       <p>
         <li>
-          The Gallery Link will Load random list of movies from external API.
+          The Gallery Link will Load random list of movies from external API{" "}
+          <span
+            style={{
+              background: "red",
+              padding: "3px",
+              color: "#e28b8b",
+              backgroundColor: "rgba(139, 124, 124, 0.5)"
+            }}
+          >
+            https://api.mockaroo.com/api/671ec130?key=30660820
+          </span>
         </li>
         <li>Then Cache the movies after its loaded for the first time.</li>
         <li>
@@ -36,14 +32,6 @@ const Home = () => {
           the movie name. Or all combined.
         </li>
       </p>
-      {/*<p>
-        For testing: To fetch data from internal API, click the button below
-      </p>
-      <button onClick={fetchData}>Get data from backend</button>
-      <p>
-        <pre ref={jsonRef}></pre>
-      </p>*/}
-      
     </div>
   );
 };
